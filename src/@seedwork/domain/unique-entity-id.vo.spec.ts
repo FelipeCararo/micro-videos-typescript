5a85392b4eee07593619ae2416ea6d5b26f274e8
FelipeCararo <felipe.gorges.cararo@gmail.com>
2022-09-28 14:48:45 -0300

    feat: new configs

---
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,25 @@
+import InvalidUuidError from "../../@seedwork/errors/invalid-uuid.error";
+import UniqueEntityId from "./unique-entity-id.vo";
+import { validate as uuidValidate } from "uuid";
+
+describe("UniqueEntityId unique tests", () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+  it("should throw error when uuid is invalid", () => {
+    expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept uuid passed in constructor", () => {
+    const uuid = "f6a17767-184a-4f12-8d51-29463077e276";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept uuid passed in constructor", () => {
+    const uuid = "f6a17767-184a-4f12-8d51-29463077e276";
+    const vo = new UniqueEntityId(uuid);
+    expect(uuidValidate(vo.id)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
